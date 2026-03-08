745d2f0cf5449c9e73ba2b050805061e881fc8e6
khushbooverma2197 <khushbooverma2197@gmail.com>
2026-03-08 15:37:09 +0530

    Update courses.js

--- routes/courses.js
+++ routes/courses.js
@@ -1,12 +1,20 @@
-outer.get("/courses/:id/enrollments", async (req, res) => {
+import express from "express";
+import supabase from "../supabaseClient.js";
+
+const router = express.Router();
+
+router.get("/courses/:id/enrollments", async (req, res) => {
  const { id } = req.params;
  const { data, error } = await supabase
    .from("enrollments")
    .select("student_name, course_id")
    .eq("course_id", id);
+
  if (error) {
    return res.status(500).json({ error: error.message });
  }
+
  res.json(data);
 });
+
 export default router;
